f5fcf145e7d91fed3a586d2fb5053da4fb383ef9
AtharvGaur <atharava112@gmail.com>
2025-08-02 23:21:58 +0530

    commited navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import LiveStatus from "./liveStatus";
 // import {
 //   ChevronDown,
 //   FileText,
@@ -27,13 +28,9 @@ async function Navbar() {
     <div className="w-full fixed top-0 border-b dark:bg-black bg-white backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex justify-between w-full p-2 items-center">
         <Link href="/">
-          <Image
-            src="/logo.svg"
-            alt="logo"
-            height={200}
-            width={200}
-            className="h-12 py-1 w-auto object-contain"
-          />
+          <div className="rounded-md h-10 w-10 flex justify-center items-center dark:bg-white bg-black">
+            <Image src={'./ipsum.svg'} alt="logo" height={32} width={32}/>
+          </div>
         </Link>
         <div className="flex  gap-3">
           {/* <SignedIn>
@@ -79,7 +76,8 @@ async function Navbar() {
           </SignedIn> */}
           {/* <SignedOut> */}
             {/* <SignInButton> */}
-              <Button variant={"outline"}>Sign In</Button>
+            <LiveStatus/>
+              {/* <Button variant={"outline"}>Sign In</Button> */}
             {/* </SignInButton> */}
           {/* </SignedOut> */}
           {/* <SignedIn>
