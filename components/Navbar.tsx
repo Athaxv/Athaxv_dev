0c1f2a694c740b22421d4ffb5be1122c00f4d7ac
AtharvGaur <atharava112@gmail.com>
2025-08-03 23:46:38 +0530

    Done with the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,10 +25,10 @@ import LiveStatus from "./liveStatus";
 async function Navbar() {
 //   await checkUser()
   return (
-    <div className="w-full fixed top-0 border-b dark:bg-black bg-white backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+    <div className="w-full fixed top-0 dark:bg-black bg-white backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex justify-between w-full p-2 items-center">
         <Link href="/">
-          <div className="rounded-md h-10 w-10 flex justify-center items-center dark:bg-white bg-black">
+          <div className="rounded-md h-10 w-10 flex justify-center items-center dark:bg-black bg-white">
             <Image src={'./ipsum.svg'} alt="logo" height={32} width={32}/>
           </div>
         </Link>
